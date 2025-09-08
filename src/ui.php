f52d9dc8ebfcaad0843f3ee07f68cf7d4243a939
Gustavo Savio <gustavo.savio@outlook.com.br>
2025-09-08 17:02:04 -0300

    refactor:fixes choices selection

--- src/ui.php
+++ src/ui.php
@@ -4,12 +4,12 @@ function showMenu(){
     echo "\n--------Banco Azul--------\n";
     echo "[1] Consultar Saldo\n";
     echo "[2] Realizar um depósito\n";
-    echo "[3] Transferência entre contas\n";
-    echo "[4] Realizar saque\n";
-    echo "[5] Sair do banco\n";
+    echo "[3] Realizar saque\n";
+    echo "[4] Sair do banco\n";
 }
 
 function readName(){
+    echo "Digite o nome do titular da conta: ";
     $name = trim(fgets(STDIN));
     return $name;
 }
