584eb93595fd2f9eb3546ede18ba97d066c6078e
Gustavo Savio <gustavo.savio@outlook.com.br>
2025-09-09 01:48:53 -0300

    refactor:change args account -> user

--- src/operacoes.php
+++ src/operacoes.php
@@ -1,23 +1,22 @@
 <?php
-function deposit(&$account,$value){
+function deposit(&$user,$value){
     if($value > 0){
-        $account['Bank balance'] += $value;
-        echo "Depósito de R$ $value realizado com sucesso.\n";
+        $user['Bank balance'] += $value;
+        echo "Depósito de R$$value, realizado com sucesso.\n";
     } else {
         echo "ERROR: Valor de depósito inválido.\n";
     }
 }
 
-function withdraw(&$account,$value){
-    if($value > 0 && $value <= $account['Bank balance']){
-        $account['Bank balance'] -= $value;
-        echo "Saque de R$ $value realizado com sucesso.\n";
+function withdraw(&$user,$value){
+    if($value > 0 && $value <= $user['Bank balance']){
+        $user['Bank balance'] -= $value;
+        echo "Saque de R$$value, realizado com sucesso.\n";
     }else {
         echo "ERROR: Saque negado. Verifique o saldo\n";
     }
 }
 
-function showBalance($account){
-    echo "{$account['Titular']}. Saldo atual: R$ " . number_format($account['Bank balance'],2,',','.') . "\n";
-    //Formatar o saldo para o padrão brasileiro.
+function showBalance($user){
+    echo "Saldo atual: R$" . number_format($user['Bank balance'],2,'.',',') . "\n";//Formata o saldo para o padrão brasileiro.
 }
