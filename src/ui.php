679495dc010b5289f64c3090ea78bd0d01c32f59
Gustavo Savio <gustavo.savio@outlook.com.br>
2025-09-09 01:50:06 -0300

    feat: new function for user menu login

--- src/ui.php
+++ src/ui.php
@@ -1,18 +1,51 @@
 <?php
 
 function showMenu(){
-    echo "\n--------Banco Azul--------\n";
-    echo "[1] Consultar Saldo\n";
-    echo "[2] Realizar um depósito\n";
-    echo "[3] Realizar saque\n";
-    echo "[4] Sair do banco\n";
+    echo "[1] Fazer login\n";
+    echo "[2] Cadastrar nova conta\n";
+    echo "[3] Sair do banco\n";
+    echo "Selecione a opção desejada: ";
 }
 
-function readName(){
+function bankMenuUser(){
+    while(true){
+        echo "=======================\n";
+        echo "[1] Consultar saldo.\n";
+        echo "[2] Realizar um depósito.\n";
+        echo "[3] Realizar saque.\n";
+        echo "[4] Sair do banco.\n";
+
+        $choice = readInput();
+        
+        switch($choice){
+            case 1:
+                showBalance($user);
+                break;
+            case 2:
+                echo "Digite o valor do depósito R$";
+                $value = (float) readInput();
+                deposit($user,$value);
+                break;
+            case 3:
+                echo "Digite o valor do saque R$";
+                $value = (float) readInput();
+                withdraw($user,$value);
+                break;
+            case 4:
+                echo "Fechando o app...";
+                sleep(2);
+                exit;
+        default:
+            echo "Opção inválida. \n";
+     }
+    }
+}
+
+/*function readName(){
     echo "Digite o nome do titular da conta: ";
     $name = trim(fgets(STDIN));
     return $name;
-}
+}*/ //Função descontinuada.
 
 
 function readInput(){
